11ce7123c5bcbf58cc25845134c41cc8c587af99
Paul <paulnyamawi18@example.com>
2024-08-26 20:44:59 +0300

    git add

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <video
+      autoplay
+      loop
+      muted
+      id="video"
+      class="z-[-1000] w-full absolute top-0 left-0 buttom-0 object-cover"
+    >
+      <source src="../assets/video/bnb.mp4" type="video/mp4" />
+    </video>
+   <Header/>
+  </main>
+</template>
+
+<script setup>
+import Header from "../components/Header.vue"
+</script>
+
+<style lang="scss" scoped></style>
