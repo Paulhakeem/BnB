cf3997fdd42e91026f0e50cc12379dbc8e5e7cd0
Paul <paulnyamawi18@example.com>
2024-08-26 21:59:07 +0300

    keep pushing

---
+++ src/components/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <!-- ========== MAIN CONTENT ========== -->
+    <main
+      id="content"
+      class="relative pt-24 max-w-3xl px-4 sm:px-6 lg:px-8 flex flex-col justify-center sm:items-center mx-auto size-full"
+    >
+      <div class="text-center py-8 px-4 sm:px-6 lg:px-8">
+        <h5 class="text-sm text-white sm:text-xl">Always Within Your Budget</h5>
+        <h2 class="mt-1 sm:mt-3 text-4xl font-bold text-white sm:text-6xl">
+          <span class="">My Holiday Deals</span>
+        </h2>
+      </div>
+      <button
+        class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gradient-to-tr from-blue to-gray text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85]"
+      >
+        Book Now
+        <i class="fa-solid fa-arrow-right"></i>
+      </button>
+    </main>
+    <!-- ========== END MAIN CONTENT ========== -->
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,16 +5,18 @@
       loop
       muted
       id="video"
-      class="z-[-1000] w-full absolute top-0 left-0 buttom-0 object-cover"
+      class="z-[-1000] min-w-full max-h-screen absolute top-0 left-0 buttom-0 object-cover"
     >
       <source src="../assets/video/bnb.mp4" type="video/mp4" />
     </video>
    <Header/>
+   <HomePage/>
   </main>
 </template>
 
 <script setup>
 import Header from "../components/Header.vue"
+import HomePage from "../components/HomePage.vue"
 </script>
 
 <style lang="scss" scoped></style>
