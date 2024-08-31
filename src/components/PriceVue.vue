fdb322a7bd19427109721d4401439ee7347c9684
Paul <paulnyamawi18@example.com>
2024-08-31 23:08:13 +0300

    done with to do

--- src/components/PriceVue.vue
+++ src/components/PriceVue.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="mt-24 bg-blue w-full p-6">
-    <div class="flex justify-between">
+    <div class="flex justify-between gap-6">
       <div>
         <h3 class="text-white text-md font-semibold">One Bedroom Apartment</h3>
         <p class="text-sm text-white">
@@ -11,7 +11,7 @@
       <div>
         <p class="text-xs text-white pb-2"><span class="text-2xl font-bold">$25</span> Per Night</p>
         <button
-          class="inline-flex items-center px-4 py-2 bg-white text-blue text-sm font-medium rounded-md uppercase cursor-pointer gap-2"
+          class="cursor-pointer inline-flex items-center px-4 py-2 bg-white text-blue text-sm font-medium rounded-md uppercase gap-2"
         >
           book now
           <i class="fa-solid fa-forward-fast"></i>
