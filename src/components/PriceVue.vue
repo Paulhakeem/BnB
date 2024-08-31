294848ee75de7ca7a93789bf115c159a8fedb61f
Paul <paulnyamawi18@example.com>
2024-08-31 21:46:50 +0300

    adding booking section

--- src/components/PriceVue.vue
+++ src/components/PriceVue.vue
@@ -1,7 +1,24 @@
-<template>  
- <main class="mt-24 bg-blue w-full p-12">
-  
- </main>
+<template>
+  <main class="mt-24 bg-blue w-full p-6">
+    <div class="flex justify-between">
+      <div>
+        <h3 class="text-white text-md font-semibold">One Bedroom Apartment</h3>
+        <p class="text-sm text-white">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero, hic?
+        </p>
+      </div>
+
+      <div>
+        <p class="text-xs text-white pb-2"><span class="text-2xl font-bold">$25</span> Per Night</p>
+        <button
+          class="inline-flex items-center px-4 py-2 bg-white text-blue text-sm font-medium rounded-md uppercase cursor-pointer gap-2"
+        >
+          book now
+          <i class="fa-solid fa-forward-fast"></i>
+        </button>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script setup>
